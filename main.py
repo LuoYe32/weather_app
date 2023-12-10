eedb6c56b0d8903afe937229a0b32d74b923c661
Alexandra Kozma <alexandra_kozma@mail.ru>
2023-12-11 00:42:52 +0300

    weather forecast 3

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from __future__ import print_function
+
+import os
 from datetime import datetime
+
 import requests
 import swagger_client
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from swagger_client.rest import ApiException
 
-app = FastAPI(
-    title="Weather App"
-)
+app = FastAPI(title="Weather App")
 
 
-# Модели данных для запросов и ответов
-class WeatherRequest(BaseModel):
-    country: str
-    city: str
-    when: datetime
+# class WeatherRequest(BaseModel):
+#     country: str = (Query(..., description="Country"),)
+#     city: str = Query(..., description="City")
+#     when: datetime = (Query(datetime.now(), description="Datetime"),)
 
 
 class WeatherResponse(BaseModel):
@@ -25,24 +25,28 @@ class WeatherResponse(BaseModel):
     is_precipitation: bool
 
 
-# Роут для текущей погоды
-@app.post("/get_now", response_model=WeatherResponse)
-def get_now(weather_request: WeatherRequest):
-    try:
-        # Используйте функцию запроса к внешнему сервису Tomorrow.io
-        weather_data = get_weather_from_tomorrow_io(api_key="U4PjQiiqhnmTXjUJ2DJBcnGpHweby2nu",
-                                                    country=weather_request.country,
-                                                    city=weather_request.city,
-                                                    when=weather_request.when)
-        # Обработайте данные, если необходимо
-        # ...
 
-        weather_data1 = get_weather_from_weather_api(api_key="3dbe11fc94a34345887200931230612",
-                                                    country=weather_request.country,
-                                                    city=weather_request.city,
-                                                    when=weather_request.when)
-        print("Tomorrow.io Data:", weather_data)
-        print("WeatherAPI Data:", weather_data1)
+@app.get("/get_now", response_model=WeatherResponse)
+def get_now(
+    country: str = Query(..., description="Country"),
+    city: str = Query(..., description="City"),
+    when: datetime = Query(datetime.now(), description="Datetime"),
+):
+    try:
+        weather_data = get_weather_from_tomorrow_io(
+            api_key="U4PjQiiqhnmTXjUJ2DJBcnGpHweby2nu",
+            country=country,
+            city=city,
+            when=when,
+        )
+        weather_data1 = get_weather_from_weather_api(
+            api_key="3dbe11fc94a34345887200931230612",
+            country=country,
+            city=city,
+            when=when,
+        )
+        # print("Tomorrow.io Data:", weather_data)
+        # print("WeatherAPI Data:", weather_data1)
 
         response_data = {"tomorrow_io": weather_data, "weather_api": weather_data1}
 
@@ -50,20 +54,28 @@ def get_now(weather_request: WeatherRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-# Роут для прогноза погоды
-@app.post("/get_forecast", response_model=WeatherResponse)
-def get_forecast(weather_request: WeatherRequest):
-    try:
-        # Используйте функцию запроса к внешнему сервису Tomorrow.io
-        forecast_data = get_weather_forecast_from_tomorrow_io(api_key="U4PjQiiqhnmTXjUJ2DJBcnGpHweby2nu",
-                                                              country=weather_request.country,
-                                                              city=weather_request.city,
-                                                              when=weather_request.when)
 
-        forecast_data1 = get_weather_forecast_from_weather_api(api_key="3dbe11fc94a34345887200931230612",
-                                                              country=weather_request.country,
-                                                              city=weather_request.city,
-                                                              when=weather_request.when)
+
+@app.get("/get_forecast", response_model=WeatherResponse)
+def get_forecast(
+    country: str = Query(..., description="Country"),
+    city: str = Query(..., description="City"),
+    when: datetime = Query(datetime.now(), description="Datetime"),
+):
+    try:
+        forecast_data = get_weather_forecast_from_tomorrow_io(
+            api_key="U4PjQiiqhnmTXjUJ2DJBcnGpHweby2nu",
+            country=country,
+            city=city,
+            when=when,
+        )
+
+        forecast_data1 = get_weather_forecast_from_weather_api(
+            api_key="3dbe11fc94a34345887200931230612",
+            country=country,
+            city=city,
+            when=when,
+        )
 
         print("Tomorrow.io Data:", forecast_data)
         print("WeatherAPI Data:", forecast_data1)
@@ -74,10 +86,10 @@ def get_forecast(weather_request: WeatherRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
+
 def get_weather_from_tomorrow_io(api_key: str, country: str, city: str, when: datetime):
     url = f"https://api.tomorrow.io/v4/weather/realtime"
 
-    # Примерно так должен выглядеть ваш запрос
     params = {
         "location": f"{city}/{country}",
         "apikey": api_key,
@@ -90,26 +102,27 @@ def get_weather_from_tomorrow_io(api_key: str, country: str, city: str, when: da
 
     data = response.json()
 
-    # Парсинг данных из ответа
     values = data.get("data", {}).get("values", {})
     temperature = values.get("temperature", None)
 
-    # Проверка, есть ли осадки
     precipitation_probability = values.get("precipitationProbability", 0)
-    print('precipitation_probability ', precipitation_probability)
+    print("precipitation_probability ", precipitation_probability)
     precipitation = precipitation_probability > 0
 
     if temperature is not None:
         return {"temp_celsius": str(temperature), "is_precipitation": precipitation}
     else:
-        raise HTTPException(status_code=500, detail="Failed to parse temperature from Tomorrow.io response")
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to get weather data from Tomorrow.io",
+        )
 
 
-def get_weather_forecast_from_tomorrow_io(api_key: str, country: str, city: str, when: datetime):
+def get_weather_forecast_from_tomorrow_io(
+    api_key: str, country: str, city: str, when: datetime
+):
     url = f"https://api.tomorrow.io/v4/weather/forecast"
     when_s = str(when)[:10]
-    #print(when_s)
-    # Параметры URL
     params = {
         "location": f"{city}/{country}",
         "apikey": api_key,
@@ -119,72 +132,68 @@ def get_weather_forecast_from_tomorrow_io(api_key: str, country: str, city: str,
 
     response = requests.get(url, params=params, headers=headers)
     response.raise_for_status()
-    #print(response.text)
+
     data = response.json()
-    relevant_data = None
+    temperature_avg = None
+    precipitation_avg = None
     for daily_data in data.get("timelines", {}).get("daily", []):
-        # Сравниваем дату
+
         if daily_data.get("time", "")[:10] == when_s:
-            #print(daily_data.get("time", "")[:10])
-            # Извлекаем температуру и осадки
-            #values = daily_data.get("data", {}).get("values", {})
             temperature_avg = daily_data.get("values", {}).get("temperatureAvg", None)
-            precipitation_avg = daily_data.get("values", {}).get("precipitationProbabilityAvg", 0)
+            precipitation_avg = daily_data.get("values", {}).get(
+                "precipitationProbabilityAvg", 0
+            )
             precipitation_avg = precipitation_avg > 0
-            # temperature_avg = daily_data.get("temperatureAvg", None)
-            # precipitation_avg = daily_data.get("precipitationProbabilityAvg", 0)
-            #
-            # # Теперь у вас есть нужные данные
-            # relevant_data = {
-            #     "temp_celsius": temperature_avg,
-            #     "is_precipitation": precipitation_avg,
-            # }
             break
 
-    # temperature_avg = 1
-    # precipitation_avg = 1
     if temperature_avg is not None:
-        return {"temp_celsius": str(temperature_avg), "is_precipitation": precipitation_avg}
+        return {
+            "temp_celsius": str(temperature_avg),
+            "is_precipitation": precipitation_avg,
+        }
     else:
-        raise HTTPException(status_code=500, detail="Failed to parse temperature from Tomorrow.io response")
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to get weather data from Tomorrow.io",
+        )
+
 
 def get_weather_from_weather_api(api_key: str, country: str, city: str, when: datetime):
     configuration = swagger_client.Configuration()
-    configuration.api_key['key'] = '3dbe11fc94a34345887200931230612'
+    configuration.api_key["key"] = "3dbe11fc94a34345887200931230612"
 
     api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
     q = f"{city}/{country}"
 
     try:
-        # Realtime API
         api_response = api_instance.realtime_weather(q)
-        #print(api_response)
     except ApiException as e:
         print("Exception when calling APIsApi->realtime_weather: %s\n" % e)
-        raise HTTPException(status_code=500, detail="Failed to get weather data from WeatherAPI")
+        raise HTTPException(
+            status_code=500, detail="Failed to get weather data from WeatherAPI"
+        )
 
-    #data = api_response.json()
-
-    # Парсинг данных из ответа
-    values = api_response.get("current", {}) #.get("values", {})
+    values = api_response.get("current", {})  # .get("values", {})
     temperature = values.get("temp_c", None)
-    #temperature = 1
 
-    # Проверка, есть ли осадки
     precipitation_probability = values.get("precip_mm", 0)
-    print('precipitation_probability ', precipitation_probability)
-    #precipitation_probability = 1
+    print("precipitation_probability ", precipitation_probability)
     precipitation = precipitation_probability > 0
-
 
     if temperature is not None:
         return {"temp_celsius": str(temperature), "is_precipitation": precipitation}
     else:
-        raise HTTPException(status_code=500, detail="Failed to parse temperature from Tomorrow.io response")
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to get weather data from WeatherAPI",
+        )
 
-def get_weather_forecast_from_weather_api(api_key: str, country: str, city: str, when: datetime):
+
+def get_weather_forecast_from_weather_api(
+    api_key: str, country: str, city: str, when: datetime
+):
     configuration = swagger_client.Configuration()
-    configuration.api_key['key'] = '3dbe11fc94a34345887200931230612'
+    configuration.api_key["key"] = api_key
 
     api_instance = swagger_client.APIsApi(swagger_client.ApiClient(configuration))
     q = f"{city}/{country}"
@@ -194,34 +203,33 @@ def get_weather_forecast_from_weather_api(api_key: str, country: str, city: str,
     dt = str(when)[:10]
 
     try:
-        # Forecast API
         api_response = api_instance.forecast_weather(q, days, dt=dt)
-        #print(api_response)
     except ApiException as e:
         print("Exception when calling APIsApi->forecast_weather: %s\n" % e)
-        raise HTTPException(status_code=500, detail="Failed to get weather data from WeatherAPI")
-
-
-    #print(response.text)
-    #data = response.json()
-    relevant_data = None
+        raise HTTPException(
+            status_code=500, detail="Failed to get weather data from WeatherAPI"
+        )
+    temperature_avg = None
+    precipitation_avg = None
     for daily_data in api_response.get("forecast", {}).get("forecastday", []):
-        # Сравниваем дату
         if daily_data.get("date", "")[:10] == dt:
-            #print(daily_data.get("time", "")[:10])
-            # Извлекаем температуру и осадки
-            #values = daily_data.get("data", {}).get("values", {})
             temperature_avg = daily_data.get("day", {}).get("avgtemp_c", None)
             precipitation_avg = daily_data.get("day", {}).get("totalprecip_mm", 0)
             precipitation_avg = precipitation_avg > 0
             break
 
-    # temperature_avg = 1
-    # precipitation_avg = 1
     if temperature_avg is not None:
-        return {"temp_celsius": str(temperature_avg), "is_precipitation": precipitation_avg}
+        return {
+            "temp_celsius": str(temperature_avg),
+            "is_precipitation": precipitation_avg,
+        }
     else:
-        raise HTTPException(status_code=500, detail="Failed to parse temperature from Tomorrow.io response")
+        raise HTTPException(
+            status_code=500,
+            detail="Failed to get weather data from WeatherAPI",
+        )
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
